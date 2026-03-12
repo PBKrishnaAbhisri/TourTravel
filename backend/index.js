cf0fe0f29b7f7841c52c33c8840c68aabc1ac2a5
P.B.Krishna Abhisri <krishnaabhisri@gmail.com>
2026-03-12 11:37:42 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ import packageRoute from './routes/packages.js'
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000
 
 // Updated CORS options to allow Authorization header
 const corsOptions = {
